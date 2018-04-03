4b3c207a82f600e1e756afad0403bd68e0de1834
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-03 22:40:57 +0530

    Output data of console to div working
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,21 @@
  * Created by bhavyaagg on 03/04/18.
  */
 
+var console2 = console;
+console2.log = console.log;
+
+console = {};
+
+
 $(document).ready(() => {
   const $sourceCode = $('#sourceCode');
   const $runBtn = $('#runBtn');
   const $outputDiv = $('#outputDiv');
   $outputDiv.hide();
 
+  console.log = function (input) {
+    $outputDiv.show().append(input.toString());
+  }
 
   $runBtn.click(() => {
     // console.log(eval($sourceCode.val()))
@@ -19,7 +28,8 @@ $(document).ready(() => {
       })
     }
     else if (lang === 'js') {
-      console.log(2);
+      console2.log(2);
+      eval($sourceCode.val())
     }
 
   });
