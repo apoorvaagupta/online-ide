44dc5f910f01468fb3b4fdd65735cc64c4060035
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-03 22:36:55 +0530

    Working for Nodejs
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -5,13 +5,23 @@
 $(document).ready(() => {
   const $sourceCode = $('#sourceCode');
   const $runBtn = $('#runBtn');
+  const $outputDiv = $('#outputDiv');
+  $outputDiv.hide();
 
 
   $runBtn.click(() => {
     // console.log(eval($sourceCode.val()))
-    $.post('/test', {sourceCode: $sourceCode.val()}, (data) => {
-      console.log(data);
-    })
+    let lang = $('input[name="language"]:checked').val();
+    if (lang === 'node') {
+      $.post('/test', {sourceCode: $sourceCode.val()}, (data) => {
+        console.log(data)
+        $('#outputDiv').show().empty().append(data);
+      })
+    }
+    else if (lang === 'js') {
+      console.log(2);
+    }
+
   });
 
 })
--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-alert(5);
+console.log(5)
